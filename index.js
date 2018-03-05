37ba93a0ffdeb64a3eff048e5179dbfc1d49ef5a
Brie A Morales <bombibills@gmail.com>
2018-03-05 18:21:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,27 +18,5 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  // var inTooDeep = [];
-  var inTooDeep = document.getElementById('grand-node').querySelectorAll("div");
-  return inTooDeep[0].innerHTML;
-  // return document.querySelector('#grand-node div div div div');
-  // let current = inTooDeep;
-  // let next = [];
-  // let isitaString = n => (typeof n === 'string');
-  
-  // for (let i = 0; i < inTooDeep.length; i++){
-  //   if (typeof inTooDeep[i] === 'string'){
-  //     return inTooDeep[i];
-  //   }
-  // }
-  // while (current) {
-  //   if (isitaString(current)) {
-  //     return current;
-  //   }
-  //   if (Array.isArray(current)) {
-  //     for (let i = 0; i < current.length; i++) {
-  //       next.push(current[i])
-  //     }
-  //   }
-  // }
+  return document.querySelector('#grand-node div div div div');
 }
