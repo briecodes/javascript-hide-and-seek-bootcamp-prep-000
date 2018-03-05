81b89d97bcfc0ef0ab7c6dbd6a7d27ab021078fe
Brie A Morales <bombibills@gmail.com>
2018-03-05 17:53:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,43 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested');
+}
+
+function increaseRankBy(n){
+  // Option 1, using element ID (useful if need to target specific area):
+  // const listEd = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  //Option 2, using only class name (useful if general filter is OK):
+  const listEd = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < listEd.length; i++) {
+    listEd[i].innerHTML = (parseInt(listEd[i].innerHTML) + n).toString();
+  }
+}
+
+function deepestChild(){
+  var inTooDeep = [];
+  inTooDeep = document.getElementById('grand-node');
+  let current = inTooDeep;
+  let next = [];
+  let isitaString = n => (typeof n === 'string');
+  
+  // for (let i = 0; i < inTooDeep.length; i++){
+  //   if (typeof inTooDeep[i] === 'string'){
+  //     return inTooDeep[i];
+  //   }
+  // }
+  
+  while (current) {
+    if (isitaString(current)) {
+      return current;
+    }
+    if (Array.isArray(current)) {
+      for (let i = 0; i < current.length; i++) {
+        next.push(current[i])
+      }
+    }
+  }
+}
